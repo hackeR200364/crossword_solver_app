7318911052c01aaa9fbe6809bb408eabd23b6024
hackeR200364 <rupamkarmakar1238@gmail.com>
2025-03-30 14:06:53 +0530

    CSA-1 committed

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,197 @@
+import 'dart:developer';
+
+import 'package:cross_word_solver/solver.dart';
+import 'package:cross_word_solver/styles.dart';
+import 'package:cross_word_solver/utils/custom_btn.dart';
+import 'package:cross_word_solver/utils/custom_text_form_field.dart';
+import 'package:cross_word_solver/utils/snack_bar_utils.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Crossword Solver',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    super.key,
+  });
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  late TextEditingController _clueTextEditingController,
+      _patternTextEditingController;
+  CrosswordSolver solver = CrosswordSolver();
+  List<String> solutions = [];
+  bool loading = false, searched = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  loadWords() async {
+    await solver.loadWords();
+  }
+
+  @override
+  void initState() {
+    _clueTextEditingController = TextEditingController();
+    _patternTextEditingController = TextEditingController();
+    loadWords();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(
+              "Crossword Solver",
+              style: TextStyle(
+                color: AppColors.black,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              margin: EdgeInsets.only(
+                left: 10,
+                right: 10,
+                top: 5,
+                bottom: 10,
+              ),
+              child: Column(
+                children: [
+                  CustomTextField(
+                    hint: "Please Enter the clue",
+                    keyboard: TextInputType.text,
+                    textEditingController: _clueTextEditingController,
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    hint: "Please Enter the pattern",
+                    keyboard: TextInputType.text,
+                    textEditingController: _patternTextEditingController,
+                  ),
+                  SizedBox(height: 10),
+                  CustomBtn(
+                    horizontalMargin: 0,
+                    onTap: (() async {
+                      if (_clueTextEditingController.text.isNotEmpty &&
+                          _patternTextEditingController.text.isNotEmpty) {
+                        FocusScope.of(context).unfocus();
+                        setState(() {
+                          loading = true;
+                        });
+                        solutions = await solver
+                            .solveCrossword(
+                                _clueTextEditingController.text.trim(),
+                                _patternTextEditingController.text.trim())
+                            .whenComplete(() {
+                          setState(() {
+                            loading = false;
+                            searched = true;
+                          });
+                        });
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          AppSnackBar.customizedAppSnackBar(
+                            message: "Please fill the fields correctly",
+                            context: context,
+                          ),
+                        );
+                      }
+                      log(solutions.isNotEmpty
+                          ? solutions.toString()
+                          : "Empty");
+                    }),
+                    text: "Search",
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(height: 10),
+          ),
+          if (solutions.isEmpty && searched)
+            SliverToBoxAdapter(
+              child: Center(
+                child: Text("No matches found"),
+              ),
+            ),
+          if (loading)
+            SliverToBoxAdapter(
+              child: Center(
+                child: CircularProgressIndicator(
+                  color: AppColors.green,
+                ),
+              ),
+            ),
+          if (solutions.isNotEmpty && !loading)
+            SliverToBoxAdapter(
+              child: Center(
+                child: Text(
+                  "Results",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          if (solutions.isNotEmpty && !loading)
+            SliverToBoxAdapter(
+              child: SizedBox(height: 10),
+            ),
+          if (solutions.isNotEmpty && !loading)
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                ((ctx, idx) {
+                  return Container(
+                    height: 40,
+                    margin: EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      top: 5,
+                      bottom: 5,
+                    ),
+                    decoration: BoxDecoration(
+                      color: AppColors.green.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Text(
+                        solutions[idx],
+                      ),
+                    ),
+                  );
+                }),
+                childCount: solutions.length,
+              ),
+            )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
